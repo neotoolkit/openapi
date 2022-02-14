5720ead7e4245d49bbf3eead8df32c710a723126
Sasha Melentyev <sasha@melentyev.io>
2022-02-14 15:01:58 +0300

    feat(openapi): add support requestBody ref

--- requestbody.go
+++ requestbody.go
@@ -5,4 +5,5 @@ type RequestBody struct {
 	Description string  `json:"description,omitempty" yaml:"description,omitempty"`
 	Required    bool    `json:"required,omitempty" yaml:"required,omitempty"`
 	Content     Content `json:"content,omitempty" yaml:"content,omitempty"`
+	Ref         string  `json:"$ref,omitempty" yaml:"$ref,omitempty"`
 }
