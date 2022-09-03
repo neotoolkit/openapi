bc4ea0154c1b4fbf396545136ee1b1940099b0b3
Gareth Jones <gareth@garethpaul.com>
2022-09-02 20:05:37 -0700

    add Parameters to Path

--- path.go
+++ path.go
@@ -2,11 +2,12 @@ package openapi
 
 // Path -.
 type Path struct {
-	Post   *Operation `json:"post,omitempty" yaml:"post,omitempty"`
-	Get    *Operation `json:"get,omitempty" yaml:"get,omitempty"`
-	Put    *Operation `json:"put,omitempty" yaml:"put,omitempty"`
-	Patch  *Operation `json:"patch,omitempty" yaml:"patch,omitempty"`
-	Delete *Operation `json:"delete,omitempty" yaml:"delete,omitempty"`
+	Post       *Operation `json:"post,omitempty" yaml:"post,omitempty"`
+	Get        *Operation `json:"get,omitempty" yaml:"get,omitempty"`
+	Put        *Operation `json:"put,omitempty" yaml:"put,omitempty"`
+	Patch      *Operation `json:"patch,omitempty" yaml:"patch,omitempty"`
+	Delete     *Operation `json:"delete,omitempty" yaml:"delete,omitempty"`
+	Parameters Parameters `json:"parameters,omitempty" yaml:"parameters,omitempty"`
 }
 
 // Paths -.
